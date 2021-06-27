a4dec316cbdab9d32834503fd96af11a18f3c32a
atiknn <atiknaikwadi1982@gmail.com>
2021-06-27 19:56:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 require("./app/routes/customer.routes.js")(app);
 
 // set port, listen for requests
-const PORT =  3000;
+const PORT =  31297;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
