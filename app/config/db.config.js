8ca47d699b07e7e5e720bed4af34bd4b83f1a4c4
atiknn <atiknaikwadi1982@gmail.com>
2021-06-27 21:02:59 +0100

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "34.201.52.167",
-  USER: "root",
-  PASSWORD: "Devopsjenkins000$",
-  DB: "nodejs"
+  HOST: "localhost",
+  USER: "rayahn",
+  PASSWORD: "xxxxxx",
+  DB: "xxxxx"
 };
